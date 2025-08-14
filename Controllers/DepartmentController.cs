16c06d50010bee24c4721c865fbba7dc8d0373b1
ahmed ali mahdy <ahmedali.clash3@gmail.com>
2025-08-14 14:03:37 +0300

    Creating model controllers for CRUD operation and View model

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using universityManagementSys.Data;
+using universityManagementSys.Models;
+using universityManagementSys.ModelView;
+
+namespace universityManagementSys.wwwroot
+{
+    public class DepartmentController : Controller
+    {
+        Context _context;
+
+        public DepartmentController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult GetAllDepartments()
+        {
+            var departments = _context.departments.ToList();
+            if (departments == null || !departments.Any())
+            {
+                return NotFound();
+            }
+            return View(departments);
+        }
+        public IActionResult GetDepartmentByID(int id)
+        {
+            var department = _context.departments.FirstOrDefault(d => d.ID == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+        public IActionResult GetAllDepartmentByCourseId(int id)
+        {
+            var courses = _context.departmentCourses
+                .Where(dc => dc.DepartmentID == id)
+                .Select(dc => dc.Course)
+                .ToList();
+            if (courses == null || !courses.Any())
+            {
+                return NotFound();
+            }
+            return View(courses);
+        }
+        public IActionResult GetDepartmentByStudentId(int id)
+        {
+            var student = _context.students.FirstOrDefault(s => s.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            var department = _context.departments.FirstOrDefault(d => d.ID == student.DepartmentID);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+        public IActionResult Create()
+        {
+            ViewModel modelView = new ViewModel
+            {
+                PageTitle = "Create Department",
+                WelcomeMessage = "Welcome to the Department Creation Page",
+            };
+            return View(modelView);
+        }
+        public IActionResult CreateDepartment(Department department)
+        {
+            _context.departments.Add(department);
+            _context.SaveChanges();
+            TempData["Success"] = "Department added successfully!";
+            return RedirectToAction("GetAllDepartments");
+        }
+        public IActionResult Edit(int id)
+        {
+
+            var department = _context.departments.FirstOrDefault(s => s.ID == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+        public IActionResult EditDepartment(Department department)
+        {
+            _context.departments.Update(department);
+            _context.SaveChanges();
+            TempData["Success"] = "Department updated successfully!";
+            return RedirectToAction("GetAllDepartments");
+        }
+        public IActionResult Delete(int id)
+        {
+          
+            var department = _context.departments.FirstOrDefault(s => s.ID == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+        public IActionResult DeleteDepartmentConfirmed(int id)
+        {
+            var department = _context.departments.FirstOrDefault(s => s.ID == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            _context.departments.Remove(department);
+            _context.SaveChanges();
+            TempData["Success"] = "Department deleted successfully!";
+            return RedirectToAction("GetAllDepartments");
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using universityManagementSys.Data;
+using universityManagementSys.Models;
+using universityManagementSys.ModelView;
+
+namespace universityManagementSys.Controllers
+{
+    public class StudentController : Controller
+    {
+        Context _context;
+
+        public StudentController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult GetAllStudents()
+        {
+            var students = _context.students.ToList();
+            if (students == null || !students.Any())
+            {
+                return NotFound();
+            }
+            return View(students);
+        }
+        public IActionResult GetStudentByID(int id)
+        {
+            var student = _context.students.FirstOrDefault(s => s.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        public IActionResult GetAllStudentsByCourseID(int id)
+        {
+            var students = _context.enrollments
+                .Where(e => e.CourseID == id)
+                .Select(e => e.Student)
+                .ToList();
+            if (students == null || !students.Any())
+            {
+                return NotFound();
+            }
+            return View(students);
+        }
+        public IActionResult GetAllStudentsByDepartmentID(int id)
+        {
+            var students = _context.students
+                .Where(s => s.DepartmentID == id)
+                .ToList();
+            if (students == null || !students.Any())
+            {
+                return NotFound();
+            }
+            return View(students);
+        }
+        public IActionResult GetAllStudentsByGradeId(int id)
+        {
+            var students = _context.enrollments
+                .Where(e => e.GradeID == id)
+                .Select(e => e.Student)
+                .ToList();
+            if (students == null || !students.Any())
+            {
+                return NotFound();
+            }
+            return View(students);
+        }
+        public IActionResult Create()
+        {
+            ViewModel modelView = new ViewModel
+            {
+                PageTitle = "Create Student",
+                WelcomeMessage = "Welcome to the Student Creation Page",
+            };
+            return View(modelView);
+        }
+        public IActionResult CreateStudent(Student student)
+        {
+            _context.students.Add(student);
+            _context.SaveChanges();
+            TempData["Success"] = "Student added successfully!";
+            return RedirectToAction("GetAllStudents");
+        }
+        public IActionResult Edit(int id)
+        {
+            ViewModel modelView = new ViewModel
+            {
+                PageTitle = "Edit Student",
+                WelcomeMessage = "Welcome to the Student Edit Page",
+            };
+            var student = _context.students.FirstOrDefault(s => s.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        public IActionResult EditStudent(Student student)
+        {
+            _context.students.Update(student);
+            _context.SaveChanges();
+            TempData["Success"] = "Student updated successfully!";
+            return RedirectToAction("GetAllStudents");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = _context.students.FirstOrDefault(s => s.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        public IActionResult DeleteStudentConfirmed(int id)
+        {
+            var student = _context.students.FirstOrDefault(s => s.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.students.Remove(student);
+            _context.SaveChanges();
+            TempData["Success"] = "Student deleted successfully!";
+            return RedirectToAction("GetAllStudents");
+        }
+    }
+}
